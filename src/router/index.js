ffd0a2755d3dbc6a30538c2f96191023dc334892
tapkichvuong <huytruongminh44@gmail.com>
2022-10-31 10:32:13 +0700

    tao trang them lien he

--- src/router/index.js
+++ src/router/index.js
@@ -18,6 +18,12 @@ const routes = [
         name:"contact.edit",
         component:()=>import("@/views/ContactEdit.vue"),
         props: true
+    },
+    {
+        path:"/contacts/",
+        name:"contact.add",
+        component:()=>import("@/views/ContactAdd.vue"),
+        props: true
     }
 ];
 
